b8eb3d550fa64fd71abb2b854e72590ecf5a0697
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2024-10-14 11:00:09 +0100

    create sign-in page + create  sign-up page

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { ReactNode } from "react";
+
+interface AuthLayoutProps {
+  children: ReactNode;
+}
+
+export default function Authlayout({ children }: AuthLayoutProps) {
+  return (
+    <main className="bg-neutral-100 min-h-screen ">
+      <div className="mx-auto max-w-screen-2xl p-4 ">
+        <nav className="flex justify-between items-center ">
+          <Image src="/logo.svg" alt="logo" width={152} height={56} />
+          <Button variant="secondary">Sign Up</Button>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,5 @@
+// type Props = {};
+
+export default function SignInPage() {
+  return <div>sign in page</div>;
+}
