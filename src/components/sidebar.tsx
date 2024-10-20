faa1980a3a0ab43d85f09edb28a228ee470fd118
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2024-10-20 19:03:53 +0100

    change logo

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,13 +3,14 @@ import Link from "next/link";
 import { DottedSeparator } from "./doted-separator";
 import Navigation from "./navigation";
 
-type Props = {};
-
-function Sidebar({}: Props) {
+function Sidebar() {
   return (
     <aside className="h-full bg-neutral-100 p-4 w-full ">
       <Link href="/">
-        <Image src="/logo.svg" alt="logo" width={50} height={50} />
+        <div className="flex items-center gap-x-2">
+          <Image src="/logo.svg" alt="logo" width={50} height={50} />
+          <p>FUTO-TEAM</p>
+        </div>
       </Link>
       <DottedSeparator className="my-4" />
       <Navigation />
